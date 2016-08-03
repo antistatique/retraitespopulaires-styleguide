699b9f347a25fda1e9c34d753ead00fa35cffcc7
Kevin Wenger <kevin@antistatique.net>
2016-08-03 19:51:59 +0200

    Add random documents/faqs by default

--- web/modules/custom/retraitespopulaires/rp_site/src/Plugin/Block/DocumentsBlock.php
+++ web/modules/custom/retraitespopulaires/rp_site/src/Plugin/Block/DocumentsBlock.php
@@ -15,6 +15,7 @@ use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Routing\CurrentRouteMatch;
 use Drupal\Core\Path\AliasManagerInterface;
 use Drupal\rp_site\Service\Profession;
+use Drupal\Core\Entity\Query\QueryFactory;
 
 /**
 * Provides a 'Useful Documents' Block
@@ -56,14 +57,21 @@ class DocumentsBlock extends BlockBase implements ContainerFactoryPluginInterfac
     private $profession;
 
     /**
+    * QueryFactory to execute query
+    * @var QueryFactory
+    */
+    private $entity_query;
+
+    /**
     * Class constructor.
     */
-    public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity, CurrentRouteMatch $route, AliasManagerInterface $alias_manager, Profession $profession) {
+    public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity, CurrentRouteMatch $route, AliasManagerInterface $alias_manager, Profession $profession, QueryFactory $query) {
         parent::__construct($configuration, $plugin_id, $plugin_definition);
         $this->entity_node   = $entity->getStorage('node');
         $this->route         = $route;
         $this->alias_manager = $alias_manager;
         $this->profession    = $profession;
+        $this->entity_query  = $query;
     }
 
     /**
@@ -80,7 +88,8 @@ class DocumentsBlock extends BlockBase implements ContainerFactoryPluginInterfac
             $container->get('entity_type.manager'),
             $container->get('current_route_match'),
             $container->get('path.alias_manager'),
-            $container->get('rp_site.profession')
+            $container->get('rp_site.profession'),
+            $container->get('entity.query')
         );
     }
 
@@ -92,26 +101,37 @@ class DocumentsBlock extends BlockBase implements ContainerFactoryPluginInterfac
         //Load the current node's field_document
         $documents_nids = array();
         if ($node = $this->route->getParameter('node')) {
-            if( isset($node->field_document) && !empty($node->field_document) ){
+
+            if( isset($node->field_document) && !$node->field_document->isEmpty() ){
+                // Retrieve specified documents
                 foreach ($node->field_document as $key => $doc) {
                     $documents_nids[] = $doc->target_id;
                 }
                 $variables['documents'] = $this->entity_node->loadMultiple($documents_nids);
+            } else {
+                // Retrieve random documents
+                $query = $this->entity_query->get('node')
+                    ->condition('type', 'document')
+                    ->condition('status', 1)
+                    ->condition('field_profession', $node->field_profession->target_id)
+                    ->addTag('random')
+                    ->range(0, 3);
 
-                if (empty($node->field_profession)) { return; }
-
-                $alias = $this->alias_manager->getAliasByPath('/taxonomy/term/'.$node->field_profession->target_id);
-                if( !empty($alias) ){
-                    $alias = str_replace('/', '', $alias);
-                }
-                $variables['collection'] = array(
-                    'name' => $this->profession->name($node->field_profession->target_id),
-                    'link' => Url::fromRoute('rp_site.documents.collection', array('taxonomy_term_alias' => $alias))
-                );
+                $nids = $query->execute();
+                $variables['documents'] = $this->entity_node->loadMultiple($nids);
             }
-        }
 
-        if (empty($variables['documents'])) { return; }
+            // Generate the collection link
+            $alias = $this->alias_manager->getAliasByPath('/taxonomy/term/'.$node->field_profession->target_id);
+            if( !empty($alias) ){
+                $alias = str_replace('/', '', $alias);
+            }
+
+            $variables['collection'] = array(
+                'name' => $this->profession->name($node->field_profession->target_id),
+                'link' => Url::fromRoute('rp_site.documents.collection', array('taxonomy_term_alias' => $alias))
+            );
+        }
 
         return [
             '#theme'     => 'rp_site_documents_block',
@@ -123,4 +143,88 @@ class DocumentsBlock extends BlockBase implements ContainerFactoryPluginInterfac
             ]
         ];
     }
+}
+
+function var_debug($variable, $depth=10, $strlen=100, $width=25, $i=0, &$objects = array()) {
+$search = array("", "a", "", "", "
+", "
+", "	", "");
+$replace = array('', 'a', '', '', '
+', '
+', '	', '');
+
+$string = '';
+
+switch(gettype($variable)) {
+  case 'boolean':      $string.= $variable?'true':'false'; break;
+  case 'integer':      $string.= $variable;                break;
+  case 'double':       $string.= $variable;                break;
+  case 'resource':     $string.= '[resource]';             break;
+  case 'NULL':         $string.= "null";                   break;
+  case 'unknown type': $string.= '???';                    break;
+  case 'string':
+    $len = strlen($variable);
+    $variable = str_replace($search,$replace,substr($variable,0,$strlen),$count);
+    $variable = substr($variable,0,$strlen);
+    if ($len<$strlen) $string.= '"'.$variable.'"';
+    else $string.= 'string('.$len.'): "'.$variable.'"...';
+    break;
+  case 'array':
+    $len = count($variable);
+    if ($i==$depth) $string.= 'array('.$len.') {...}';
+    elseif(!$len) $string.= 'array(0) {}';
+    else {
+      $keys = array_keys($variable);
+      $spaces = str_repeat(' ',$i*2);
+      $string.= "array($len)
+".$spaces.'{';
+      $count=0;
+      foreach($keys as $key) {
+        if ($count==$width) {
+          $string.= "
+".$spaces."  ...";
+          break;
+        }
+        $string.= "
+".$spaces."  [$key] => ";
+        $string.= var_debug($variable[$key],$strlen,$width,$depth,$i+1,$objects);
+        $count++;
+      }
+      $string.="
+".$spaces.'}';
+    }
+    break;
+  case 'object':
+    $id = array_search($variable,$objects,true);
+    if ($id!==false)
+      $string.=get_class($variable).'#'.($id+1).' {...}';
+    else if($i==$depth)
+      $string.=get_class($variable).' {...}';
+    else {
+      $id = array_push($objects,$variable);
+      $array = (array)$variable;
+      $spaces = str_repeat(' ',$i*2);
+      $string.= get_class($variable)."#$id
+".$spaces.'{';
+      $properties = array_keys($array);
+      foreach($properties as $property) {
+        $name = str_replace("",':',trim($property));
+        $string.= "
+".$spaces."  [$name] => ";
+        $string.= var_debug($array[$property],$strlen,$width,$depth,$i+1,$objects);
+      }
+      $string.= "
+".$spaces.'}';
+    }
+    break;
+}
+
+if ($i>0) return $string;
+
+$backtrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+do $caller = array_shift($backtrace); while ($caller && !isset($caller['file']));
+if ($caller) $string = $caller['file'].':'.$caller['line']."
+".$string;
+
+echo $string;
 }
--- web/modules/custom/retraitespopulaires/rp_site/src/Plugin/Block/FAQsBlock.php
+++ web/modules/custom/retraitespopulaires/rp_site/src/Plugin/Block/FAQsBlock.php
@@ -15,6 +15,7 @@ use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Routing\CurrentRouteMatch;
 use Drupal\Core\Path\AliasManagerInterface;
 use Drupal\rp_site\Service\Profession;
+use Drupal\Core\Entity\Query\QueryFactory;
 
 /**
 * Provides a 'FAQs' Block
@@ -58,12 +59,13 @@ class FAQsBlock extends BlockBase implements ContainerFactoryPluginInterface {
     /**
     * Class constructor.
     */
-    public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity, CurrentRouteMatch $route, AliasManagerInterface $alias_manager, Profession $profession) {
+    public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity, CurrentRouteMatch $route, AliasManagerInterface $alias_manager, Profession $profession, QueryFactory $query) {
         parent::__construct($configuration, $plugin_id, $plugin_definition);
         $this->entity_node   = $entity->getStorage('node');
         $this->route         = $route;
         $this->alias_manager = $alias_manager;
         $this->profession    = $profession;
+        $this->entity_query  = $query;
     }
 
     /**
@@ -80,7 +82,8 @@ class FAQsBlock extends BlockBase implements ContainerFactoryPluginInterface {
             $container->get('entity_type.manager'),
             $container->get('current_route_match'),
             $container->get('path.alias_manager'),
-            $container->get('rp_site.profession')
+            $container->get('rp_site.profession'),
+            $container->get('entity.query')
         );
     }
 
@@ -93,27 +96,36 @@ class FAQsBlock extends BlockBase implements ContainerFactoryPluginInterface {
         $faqs_nids = array();
         if ($node = $this->route->getParameter('node')) {
 
-            if( isset($node->field_faq) && !empty($node->field_faq) ){
+            if( isset($node->field_faq) && !$node->field_faq->isEmpty() ){
+                // Retrieve specified documents
                 foreach ($node->field_faq as $key => $doc) {
                     $faqs_nids[] = $doc->target_id;
                 }
 
                 $variables['faqs'] = $this->entity_node->loadMultiple($faqs_nids);
+            } else {
+                // Retrieve random documents
+                $query = $this->entity_query->get('node')
+                    ->condition('type', 'faq')
+                    ->condition('status', 1)
+                    ->condition('field_profession', $node->field_profession->target_id)
+                    ->addTag('random')
+                    ->range(0, 3);
 
-                if (empty($node->field_profession)) { return; }
-
-                $alias = $this->alias_manager->getAliasByPath('/taxonomy/term/'.$node->field_profession->target_id);
-                if( !empty($alias) ){
-                    $alias = str_replace('/', '', $alias);
-                }
-                $variables['collection'] = array(
-                    'name' => $this->profession->name($node->field_profession->target_id),
-                    'link' => Url::fromRoute('rp_site.faqs.collection', array('taxonomy_term_alias' => $alias))
-                );
+                $nids = $query->execute();
+                $variables['faqs'] = $this->entity_node->loadMultiple($nids);
             }
-        }
 
-        if (empty($variables['faqs'])) { return; }
+            // Generate the collection link
+            $alias = $this->alias_manager->getAliasByPath('/taxonomy/term/'.$node->field_profession->target_id);
+            if( !empty($alias) ){
+                $alias = str_replace('/', '', $alias);
+            }
+            $variables['collection'] = array(
+                'name' => $this->profession->name($node->field_profession->target_id),
+                'link' => Url::fromRoute('rp_site.faqs.collection', array('taxonomy_term_alias' => $alias))
+            );
+        }
 
         return [
             '#theme'     => 'rp_site_faqs_block',
