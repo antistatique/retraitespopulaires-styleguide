283c0e0a3fe6c52b6079f0f2003c6c583794b423
Kevin Wenger <kevin@antistatique.net>
2016-10-14 13:35:27 +0200

    Inite blocks contacts

---
+++ web/modules/custom/retraitespopulaires/rp_site/src/Plugin/Block/ContactPlaceholderTeaserBlock.php
@@ -0,0 +1,39 @@
+<?php
+/**
+* @file
+* Contains \Drupal\rp_site\Plugin\Block\ContactPlaceholderTeaser.
+*/
+
+namespace Drupal\rp_site\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+* Provides a 'Contact Placeholder Teaser' Block
+*
+* @Block(
+*   id = "rp_site_contact_placeholder_teaser_block",
+*   admin_label = @Translation("Contact Placeholder Teaser block"),
+* )
+*
+* Inline example:
+* <code>
+* load_block('rp_site_contact_placeholder_teaser_block')
+* </code>
+*/
+class ContactPlaceholderTeaser extends BlockBase {
+    /**
+    * {@inheritdoc}
+    */
+    public function build($params = array()) {
+        $variables = array();
+        $variables = $params;
+
+        return [
+            '#theme'     => 'rp_site_contact_placeholder_teaser_block',
+            '#variables' => $variables,
+        ];
+    }
+}
---
+++ web/modules/custom/retraitespopulaires/rp_site/src/Plugin/Block/ContactTeaserBlock.php
@@ -0,0 +1,39 @@
+<?php
+/**
+* @file
+* Contains \Drupal\rp_site\Plugin\Block\ContactTeaser.
+*/
+
+namespace Drupal\rp_site\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+* Provides a 'Contact  Teaser' Block
+*
+* @Block(
+*   id = "rp_site_contact_teaser_block",
+*   admin_label = @Translation("Contact  Teaser block"),
+* )
+*
+* Inline example:
+* <code>
+* load_block('rp_site_contact_teaser_block')
+* </code>
+*/
+class ContactTeaser extends BlockBase {
+    /**
+    * {@inheritdoc}
+    */
+    public function build($params = array()) {
+        $variables = array();
+        $variables = $params;
+
+        return [
+            '#theme'     => 'rp_site_contact_teaser_block',
+            '#variables' => $variables,
+        ];
+    }
+}
