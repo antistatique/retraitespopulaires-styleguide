dd7b06e59a26537f6112cb33ba16442ca76089e7
Gilles Doge <gilles@antistatique.net>
2016-10-18 00:15:07 +0200

    complete Mortgage calculator

--- web/modules/custom/retraitespopulaires/rp_mortgage/src/Calculator/MortgageCalculator.php
+++ web/modules/custom/retraitespopulaires/rp_mortgage/src/Calculator/MortgageCalculator.php
@@ -43,20 +43,30 @@ class MortgageCalculator
      * @var float
      */
     private $notaryRateFee;
+    /**
+     * @var float
+     */
+    private $amortisationFirstRate;
+    /**
+     * @var float
+     */
+    private $amortistationSecondRate;
 
     /**
      * MortgageCalculator constructor.
      *
-     * @param float $notaryRateFee            Droit de mutation et frais de notaire (en %)
-     * @param float $firstRateMax             Prêt max. en 1er rang par rapport au prix d'achat (en %)
-     * @param float $theoricalCostFirstRate   Charges théoriques du prêt en 1er rang (en %)
-     * @param float $secondRateMax            Prêt max. en 2ème rang par rapport au prix d'achat (en %)
-     * @param float $theoricalCostSecondRate  Charges théoriques du prêt en 2ème rang (en %)
-     * @param float $ratioCostIncomeMax       Rapport charges-revenus max. (en %)
-     * @param int   $maintenanceFees          Frais d'entretien
-     * @param float $equityCapitalMinRate     Pourcentage min. des fonds propres
-     * @param float $avgRate                  Taux moyen (1er + 2ème rang) de charges du prêt total (en %)
-     * @param float $advanceRateMax           Taux d'avance max. (en %)
+     * @param float $notaryRateFee           Droit de mutation et frais de notaire (en %)
+     * @param float $firstRateMax            Prêt max. en 1er rang par rapport au prix d'achat (en %)
+     * @param float $theoricalCostFirstRate  Charges théoriques du prêt en 1er rang (en %)
+     * @param float $secondRateMax           Prêt max. en 2ème rang par rapport au prix d'achat (en %)
+     * @param float $theoricalCostSecondRate Charges théoriques du prêt en 2ème rang (en %)
+     * @param float $ratioCostIncomeMax      Rapport charges-revenus max. (en %)
+     * @param int   $maintenanceFees         Frais d'entretien
+     * @param float $equityCapitalMinRate    Pourcentage min. des fonds propres
+     * @param float $avgRate                 Taux moyen (1er + 2ème rang) de charges du prêt total (en %)
+     * @param float $advanceRateMax          Taux d'avance max. (en %)
+     * @param float $amortisationFirstRate   Amortissement du prêt en 1er rang (en %)
+     * @param float $amortistationSecondRate Amortissement du prêt en 2ème rang (en %)
      */
     public function __construct(
         $notaryRateFee = 0.05,
@@ -68,7 +78,9 @@ class MortgageCalculator
         $maintenanceFees = 6000,
         $equityCapitalMinRate = 0.25,
         $avgRate = 0.0525,
-        $advanceRateMax = 0.80
+        $advanceRateMax = 0.80,
+        $amortisationFirstRate = 0.01,
+        $amortistationSecondRate = 0.02
     ) {
 
         $this->firstRateMax = $firstRateMax;
@@ -81,6 +93,8 @@ class MortgageCalculator
         $this->avgRate = $avgRate;
         $this->advanceRateMax = $advanceRateMax;
         $this->notaryRateFee = $notaryRateFee;
+        $this->amortisationFirstRate = $amortisationFirstRate;
+        $this->amortistationSecondRate = $amortistationSecondRate;
     }
 
     /**
@@ -88,8 +102,11 @@ class MortgageCalculator
      * @param int   $equityCapital
      * @param int   $income
      * @param float $firstRate
+     * @param float $secondRate
+     *
+     * @return \Drupal\rp_mortgage\Calculator\MortgageResult
      */
-    public function calculate($buyPrice, $equityCapital, $income, $firstRate)
+    public function calculate($buyPrice, $equityCapital, $income, $firstRate, $secondRate)
     {
         $buyPrice = (int) $buyPrice;
         $equityCapital = (int) $equityCapital;
@@ -101,16 +118,38 @@ class MortgageCalculator
         $priceMinimalEquityCapital = $this->getMinimalEquityCapital($buyPrice);
 
         $equityBuyPriceMax = $this->getMaximumBuyPriceFromEquityCapital($equityCapital);
-        $equityMinialAnnualIncome = $this->getMinimalAnnualIncomeFromEquityCapital($equityCapital);
+        $equityMinimalAnnualIncome = $this->getMinimalAnnualIncome($equityBuyPriceMax);
 
         $incomeBuyPriceMax = $this->getMaximumBuyPriceFromIncome($income);
-        $incomeMinimalAnnualIncome = $this->getMinimalAnnualIncomeFromIncome($income);
+        $incomeMinimalEquityCapital = $this->getMinimalEquityCapitalFromIncome($income);
 
         $firstLoan = $this->calculateLoan($priceWithNotaryFee, $buyPrice, $equityCapital, $this->firstRateMax);
-        $secondLoan = $this->calculateLoan($priceWithNotaryFee, $buyPrice, $firstRate, $this->secondRateMax); // @TODO: Double check this one with RP!
+        $secondLoan = $this->calculateLoan($priceWithNotaryFee, $buyPrice, ($equityCapital + $firstLoan), $this->secondRateMax); // @TODO: Double check this one with RP!
+        $totalLoan = $firstLoan + $secondLoan;
 
-        // @TODO return a Result Object
-        return $firstLoan;
+        $deptRatio = round($totalLoan / $buyPrice, 3);
+        $costIncomeRatio = (($firstLoan * $this->theoricalCostFirstRate) + ($secondLoan * $this->theoricalCostSecondRate) + $this->maintenanceFees) / $income;
+
+        $interestFirstRate = round($firstLoan * $firstRate, 0);
+        $interestSecondRate = round($secondLoan * $secondRate, 0);
+
+        $firstAmortisation = $firstLoan * $this->amortisationFirstRate;
+        $secondAmortisation = $secondLoan * $this->amortistationSecondRate;
+
+        $totalLivingCostPerYear = $interestFirstRate + $interestSecondRate + $firstAmortisation + $secondAmortisation + $this->maintenanceFees;
+
+        $result = new MortgageResult($firstLoan, $secondLoan, $totalLoan, $firstRate, $secondRate);
+        $result->setMaintenanceFees($this->maintenanceFees);
+        $result->setDeptRatio($deptRatio);
+        $result->setCostIncomeRatio($costIncomeRatio);
+        $result->setInterestFirstRate($interestFirstRate);
+        $result->setInterestSecondRate($interestSecondRate);
+        $result->setFirstAmortisation($firstAmortisation);
+        $result->setSecondAmortisation($secondAmortisation);
+        $result->setTotalLivingCostPerYear($totalLivingCostPerYear);
+        $result->setTotalLivingCostPerMonth($totalLivingCostPerYear / 12);
+
+        return $result;
     }
 
     protected function calculateLoan($priceWithNotaryFee, $price, $equityCapital, $rate)
@@ -150,7 +189,7 @@ class MortgageCalculator
      */
     public function getMinimalEquityCapital($price)
     {
-        return $price * $this->equityCapitalMinRate;
+        return round($price * $this->equityCapitalMinRate, 0);
     }
 
     /**
@@ -160,17 +199,7 @@ class MortgageCalculator
      */
     public function getMaximumBuyPriceFromEquityCapital($equityCapital)
     {
-        return $equityCapital / $this->equityCapitalMinRate;
-    }
-
-    /**
-     * @param integer|float $equityCapital
-     *
-     * @return float
-     */
-    public function getMinimalAnnualIncomeFromEquityCapital($equityCapital)
-    {
-        return $this->getMinimalAnnualIncome($equityCapital);
+        return round($equityCapital / $this->equityCapitalMinRate, 0);
     }
 
     /**
@@ -180,9 +209,10 @@ class MortgageCalculator
      */
     public function getMaximumBuyPriceFromIncome($income)
     {
-        $maxBuyPrice = ((($income / 10.0 * $this->ratioCostIncomeMax) - $this->maintenanceFees) / $this->avgRate) / $this->advanceRateMax;
+        // @TODO: What is this magic number ?
+        $maxBuyPrice = (((($income / 10.0) * 3) - $this->maintenanceFees) / $this->avgRate) / $this->advanceRateMax;
 
-        return $maxBuyPrice;
+        return round($maxBuyPrice, 0);
     }
 
     /**
@@ -190,10 +220,10 @@ class MortgageCalculator
      *
      * @return float
      */
-    public function getMinimalAnnualIncomeFromIncome($income)
+    public function getMinimalEquityCapitalFromIncome($income)
     {
         $buyPriceMax = $this->getMaximumBuyPriceFromIncome($income);
 
-        return $buyPriceMax * $this->equityCapitalMinRate;
+        return round($buyPriceMax * $this->equityCapitalMinRate, 0);
     }
 }
