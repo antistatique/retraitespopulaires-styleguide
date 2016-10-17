0af8c7411f00d72c9b4636c5073e608f088f73d0
Gilles Doge <gilles@antistatique.net>
2016-10-17 10:14:00 +0200

    fix styleguide script

--- bin/styleguide.sh
+++ bin/styleguide.sh
@@ -2,5 +2,5 @@
 
 echo -e "Copy styleguide to theme"
 rm -rf ${BASH_SOURCE%/*}/../web/themes/retraitespopulaires/build
-cp -rfv ${BASH_SOURCE%/*}../node_modules/@antistatique/retraitespopulaires-styleguide/build ${BASH_SOURCE%/*}/../web/themes/retraitespopulaires
+cp -rfv ${BASH_SOURCE%/*}/../node_modules/@antistatique/retraitespopulaires-styleguide/build ${BASH_SOURCE%/*}/../web/themes/retraitespopulaires
 echo -e "Finished"
