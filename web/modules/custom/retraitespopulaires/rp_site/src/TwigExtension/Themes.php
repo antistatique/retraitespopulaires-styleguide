555e212240b40e12198865370749fcd35f61270c
Kevin Wenger <kevin@antistatique.net>
2016-10-18 17:58:23 +0200

    Fix breadcrumb colors

--- web/modules/custom/retraitespopulaires/rp_site/src/TwigExtension/Themes.php
+++ web/modules/custom/retraitespopulaires/rp_site/src/TwigExtension/Themes.php
@@ -2,8 +2,15 @@
 namespace Drupal\rp_site\TwigExtension;
 
 use Drupal\rp_site\Service\Profession;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class Themes extends \Twig_Extension {
+
+    protected $container;
+
+    public function __construct(ContainerInterface $container){
+        $this->container = $container;
+    }
 
     /**
     * List of all Twig functions
@@ -13,6 +20,15 @@ class Themes extends \Twig_Extension {
             new \Twig_SimpleFilter('theme_profession', array($this, 'themeProfession')),
         ];
     }
+
+    /**
+    * List of all Twig functions
+    */
+     public function getFunctions(){
+         return array(
+             new \Twig_SimpleFunction('theme_guesser', array($this, 'themeGuesser'), array('is_safe' => array('html'))),
+         );
+     }
 
     /**
     * Unique identifier for this Twig extension
@@ -25,9 +41,23 @@ class Themes extends \Twig_Extension {
     Render a custom date format with Twig
     Use the internal helper "format_date" to render the date using the current language for texts
     */
-    public static function themeProfession($tid) {
-        $profession = new Profession();
+    public function themeProfession($tid) {
+        $profession = $this->container->get('rp_site.profession');
         return $profession->theme($tid);
+    }
+
+    /*
+    Render a custom date format with Twig
+    Use the internal helper "format_date" to render the date using the current language for texts
+    */
+    public function themeGuesser() {
+        $route = $this->container->get('current_route_match');
+        $node = $route->getParameter('node');
+        if (isset($node) && !empty($node->field_profession->entity->tid->value)) {
+            $profession = $this->container->get('rp_site.profession');
+            return $profession->theme($node->field_profession->entity->tid->value);
+        }
+        return '';
     }
 
 }
