de2932e9cedc150f6d4a11d99b22ae1773ae6ddc
Gilles Doge <gilles@antistatique.net>
2016-10-19 14:30:58 +0200

    change tests order to avoid warning about string expected, object given

--- web/modules/custom/antistatique/twig_extender/src/TwigExtension/Dates.php
+++ web/modules/custom/antistatique/twig_extender/src/TwigExtension/Dates.php
@@ -33,11 +33,11 @@ class Dates extends \Twig_Extension {
     Use the internal helper "format_date" to render the date using the current language for texts
     */
     public static function formatDate($date, $format) {
-        if ($date_format = \DateTime::createFromFormat('Y-m-d', $date)) {
-            $timestmap = strtotime($date);
-        }elseif (is_a($date, 'Drupal\Core\Datetime\DrupalDateTime') || is_a($date, 'DateTime')){
+        if (is_a($date, 'Drupal\Core\Datetime\DrupalDateTime') || is_a($date, 'DateTime')) {
             $timestmap = $date->getTimestamp();
-        }else{
+        } elseif ($date_format = \DateTime::createFromFormat('Y-m-d', $date)) {
+            $timestmap = strtotime($date);
+        } else {
             $timestmap = $date;
         }
         return format_date($timestmap, "custom", $format);
