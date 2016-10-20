445839f7b8f78bd8b22bce4fdad287587d47335c
Kevin Wenger <kevin@antistatique.net>
2016-10-20 08:39:41 +0200

    Added News Teaser Image Style

--- web/modules/custom/retraitespopulaires/rp_site/src/Plugin/Block/NewsTeaserBlock.php
+++ web/modules/custom/retraitespopulaires/rp_site/src/Plugin/Block/NewsTeaserBlock.php
@@ -10,6 +10,8 @@ use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
+use Drupal\rp_site\Service\Cover;
+
 /**
 * Provides a 'News Teaser' Block
 *
@@ -23,13 +25,43 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 * load_block('rp_site_news_teaser_block')
 * </code>
 */
-class NewsTeaserBlock extends BlockBase {
+class NewsTeaserBlock extends BlockBase implements ContainerFactoryPluginInterface {
+    /**
+     * Cover Service
+     * @var Cover
+     */
+    private $cover;
+
+     /**
+     * Class constructor.
+     */
+     public function __construct(array $configuration, $plugin_id, $plugin_definition, Cover $cover) {
+         parent::__construct($configuration, $plugin_id, $plugin_definition);
+         $this->cover  = $cover;
+     }
+
+     /**
+     * {@inheritdoc}
+     */
+     public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+         // Instantiates this form class.
+         return new static(
+             // Load the service required to construct this class.
+             $configuration,
+             $plugin_id,
+             $plugin_definition,
+             // Load customs services used in this class.
+             $container->get('rp_site.cover')
+         );
+     }
+
     /**
     * {@inheritdoc}
     */
     public function build($params = array()) {
         $variables = array();
         $variables = $params;
+        $variables['cover'] = $this->cover->generate($params['news'], array('xl' => 'rp_teaser_xl'));
 
         return [
             '#theme'     => 'rp_site_news_teaser_block',
