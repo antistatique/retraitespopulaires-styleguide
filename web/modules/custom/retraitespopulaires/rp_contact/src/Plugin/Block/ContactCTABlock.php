6f4a832859c9238166484fc5b3cda0c5a2ab9762
Gilles Doge <gilles.doge@gmail.com>
2016-10-27 15:39:18 +0200

    Fix Contact cover

--- web/modules/custom/retraitespopulaires/rp_contact/src/Plugin/Block/ContactCTABlock.php
+++ web/modules/custom/retraitespopulaires/rp_contact/src/Plugin/Block/ContactCTABlock.php
@@ -122,7 +122,7 @@ class ContactCTABlock extends BlockBase implements ContainerFactoryPluginInterfa
         }
 
         if (count($variables['contacts']) == 1) {
-            $variables['cover'] = $this->cover->fromNode($variables['contact'], array('xl' => 'rp_teaser_contact_portrait_xl'));
+          $variables['cover'] = $this->cover->fromNode($variables['contacts'][0], array('xl' => 'rp_teaser_contact_portrait_xl'));
         }
 
         return [
