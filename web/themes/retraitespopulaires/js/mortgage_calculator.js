9ab5772336403a48dad4ffaf2b99a2fbb9eef8c0
Gilles Doge <gilles@antistatique.net>
2016-10-29 19:12:20 +0200

    calculate results

--- web/themes/retraitespopulaires/js/mortgage_calculator.js
+++ web/themes/retraitespopulaires/js/mortgage_calculator.js
@@ -48,6 +48,7 @@
 
     $mortgageForm.bind('submit', function (e) {
       e.preventDefault();
+      updateResults();
     });
 
     updateAmountInputs(parseFloat($amountInput.val()));
@@ -63,7 +64,7 @@
         minimalEquityCapital = null;
 
       if (!isNaN(amount)) {
-        amountWithFee = getPrixAchatAvecFra isNotaire(amount, MORTGAGE_SETTINGS.notaryRateFee);
+        amountWithFee = getPrixAchatAvecFraisNotaire(amount, MORTGAGE_SETTINGS.notaryRateFee);
         minimalIncome = getRevenuAnnuelNetMin(
           amount,
           MORTGAGE_SETTINGS.firstRateMax,
@@ -125,6 +126,78 @@
     function updateRateInputs(firstRate, secondRate) {
       $rateInput.val(firstRate);
     }
+
+    function updateResults() {
+      var amount = parseFloat($amountInput.val());
+      var equityCapital = parseFloat($equityCapitalInput.val());
+      var income = parseFloat($incomeInput.val());
+      var firstRate = parseFloat($rateInput.val());
+      var secondRate = parseFloat($rateSelect.find(':selected').data('second-rate'));
+
+      if (!amount || isNaN(amount) || !equityCapital || isNaN(equityCapital) || !income || isNaN(income)) {
+        resetResult();
+        return;
+      }
+
+      var amountWithFee = getPrixAchatAvecFraisNotaire(amount, MORTGAGE_SETTINGS.notaryRateFee);
+
+      var firstLoan = getPretEnPremierRang(amount, amountWithFee, equityCapital, MORTGAGE_SETTINGS.firstRateMax);
+      var secondLoan = getPretEnDeuxiemeRang(amount, amountWithFee, equityCapital, firstLoan, MORTGAGE_SETTINGS.advanceRateMax);
+      var totalLoan = getFinancementHypothecaire(firstLoan, secondLoan);
+      var firstInterestLoan = getInteretsEnPremierRang(firstLoan, firstRate/100.0);
+      var secondInterestLoan = getInteretsEnDeuxiemeRang(secondLoan, secondRate/100.0);
+      var firstAmortisation = getAmortissementEnPremierRang(firstLoan, MORTGAGE_SETTINGS.amortisationFirstRate);
+      var secondAmortisation = getAmortissementEnDeuxiemeRang(secondLoan, MORTGAGE_SETTINGS.amortistationSecondRate);
+      var maintenanceFee = MORTGAGE_SETTINGS.maintenanceFees;
+      var monthlyLivingCost = getChargeMensuelle(firstInterestLoan, secondInterestLoan, firstAmortisation, secondAmortisation, maintenanceFee);
+      var yearlyLivingCost = getChargeAnnuelle(firstInterestLoan, secondInterestLoan, firstAmortisation, secondAmortisation, maintenanceFee);
+
+      $firstLoan = $('.js-first-loan');
+      $secondLoan = $('.js-second-loan');
+      $totalLoan = $('.js-total-loan');
+      $firstInterestLoan = $('.js-interest-first-loan');
+      $secondInterestLoan = $('.js-interest-second-loan');
+      $firstRate = $('.js-first-rate');
+      $secondRate = $('.js-second-rate');
+      $firstAmortisation = $('.js-first-amortisation');
+      $secondAmortisation = $('.js-second-amortisation');
+      $maintenanceFee = $('.js-maintenance-fee');
+      $monthlyLivingCost = $('.js-monthly-living-cost');
+      $yearlyLivingCost = $('.js-yearly-living-cost');
+
+
+      $firstLoan.text(formatPrice(firstLoan));
+      $secondLoan.text(formatPrice(secondLoan));
+      $totalLoan.text(formatPrice(totalLoan));
+      $firstInterestLoan.text(formatPrice(firstInterestLoan));
+      $secondInterestLoan.text(formatPrice(secondInterestLoan));
+      $firstRate.text(firstRate + '%');
+      $secondRate.text(secondRate + '%');
+      $firstAmortisation.text(formatPrice(firstAmortisation));
+      $secondAmortisation.text(formatPrice(secondAmortisation));
+      $maintenanceFee.text(formatPrice(maintenanceFee));
+      $monthlyLivingCost.text(monthlyLivingCost);
+      $yearlyLivingCost.text(yearlyLivingCost);
+    }
+
+    function resetResult() {
+      $('.js-first-loan').text('-');
+      $('.js-second-loan').text('-');
+      $('.js-total-loan').text('-');
+      $('.js-interest-first-loan').text('');
+      $('.js-interest-second-loan').text('');
+      $('.js-first-rate').text('');
+      $('.js-second-rate').text('');
+      $('.js-first-amortisation').text('');
+      $('.js-second-amortisation').text('');
+      $('.js-maintenance-fee').text('');
+      $('.js-monthly-living-cost').text('');
+      $('.js-yearly-living-cost').text('');
+    }
   });
+
+  function formatPrice(price) {
+    return price;
+  }
 
 })(jQuery, window.MORTGAGE_RATES, window.MORTGAGE_SETTINGS);
