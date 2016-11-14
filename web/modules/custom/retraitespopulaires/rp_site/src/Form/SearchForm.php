7a2c15c16d636329bf5675c556c5342844f1acb0
Kevin Wenger <kevin@antistatique.net>
2016-11-14 16:31:58 +0100

    Integrate Search Engine into Drupal

---
+++ web/modules/custom/retraitespopulaires/rp_site/src/Form/SearchForm.php
@@ -0,0 +1,70 @@
+<?php
+/**
+* @file
+* Contains \Drupal\rp_site\Form\SearchForm.
+*/
+
+namespace Drupal\rp_site\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+use Drupal\Core\Url;
+
+class SearchForm extends FormBase {
+
+    /**
+    * {@inheritdoc}.
+    */
+    public function getFormId() {
+        return 'rp_site_search_form';
+    }
+
+    /**
+    * {@inheritdoc}
+    */
+    public function buildForm(array $form, FormStateInterface $form_state, $params = NULL) {
+        $form['#action'] = Url::fromRoute('rp_site.search')->toString();
+        $form['#method'] = 'GET';
+
+        $form['form-wrapper_start'] = array(
+            '#markup' => '<div class="form-group">',
+        );
+
+        $form['input-group_start'] = array(
+            '#markup' => '<div class="input-group align-h">',
+        );
+
+        $form['q'] = array(
+            '#type'   => 'textfield',
+            '#placeholder' => $this->t('Chercher un produit, un document, un contact, ...'),
+            '#attributes'  => ['class' => array('full-width')],
+            '#required'    => true,
+        );
+
+        $form['actions']['submit'] = array(
+            '#type'        => 'submit',
+            '#attributes'  => ['class' => array('btn btn-default'), 'icon' => 'retraitespopulaires-icon retraitespopulaires-icon-search'],
+            '#prefix'      => '<span class="input-group-btn">',
+            '#suffix'      => '</span>',
+        );
+
+        $form['input-group_end'] = array(
+            '#markup' => '</div>',
+        );
+
+        $form['form-wrapper_end'] = array(
+            '#markup' => '</div>',
+        );
+
+        return $form;
+    }
+
+    /**
+    * {@inheritdoc}
+    */
+    public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    }
+}
