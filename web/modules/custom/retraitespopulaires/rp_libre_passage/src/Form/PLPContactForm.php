932bc9ec01a4e6231bc6fcda5e36a8930f5a7ec4
Kevin Wenger <kevin@antistatique.net>
2017-01-16 09:06:11 +0100

    Improved messages when errors form

--- web/modules/custom/retraitespopulaires/rp_libre_passage/src/Form/PLPContactForm.php
+++ web/modules/custom/retraitespopulaires/rp_libre_passage/src/Form/PLPContactForm.php
@@ -90,7 +90,7 @@ class PLPContactForm extends FormBase {
         }
         if (!empty($this->session->get('errors'))) {
             $form['errors'] = array(
-                '#markup' => '<div class="well well-danger well-lg"><p>'.t('Attention, des erreurs sont subvenues dans le formulaire. Merci de vérifier les champs en rouge.').'</p></div>',
+                '#markup' => '<div class="well well-danger well-lg"><p>'.t('Attention, des erreurs sont survenues dans le formulaire. Merci de vérifier les champs en rouge.').'</p></div>',
             );
         }
 
