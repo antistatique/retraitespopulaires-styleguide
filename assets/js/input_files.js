a50c21758a8e762c19e19530dcf43a730ec63b12
Kevin Wenger <wenger.kev@gmail.com>
2017-02-14 11:51:00 +0100

    Improved upload files input

--- assets/js/input_files.js
+++ assets/js/input_files.js
@@ -3,27 +3,29 @@ import $ from 'jquery';
 export function input_files () {
 
   $('.form-file').each(function() {
-    const $label  = $(this).find('label'),
-          labelVal = $label.text(),
-          $handler = $(this).find('.form-file-handler'),
-          $this = $(this);
+    const $handler = $(this).find('.form-file-handler'),
+          $this = $(this),
+          $input = $(this).find('[type="file"]');
 
-    $this.on('change', function(e) {
-      let fileName = '';
-      if( this.files && this.files.length > 1 ) {
-        fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+    $this.on('change', function() {
+      let files = $input[0].files;
+      let $files = $(this).find('.files');
+      if ($files.length == 0) {
+        $('<div class="files"></div>').insertAfter($handler);
+        $files = $(this).find('.files');
+      } else {
+        // Clean the list
+        $files.html('');
+      }
+
+      if( files.length == 1 ) {
+        $files.html('<small><i class="retraitespopulaires-icon retraitespopulaires-icon-documents-thick" aria-hidden="true"></i> <span>' +files[0].name +'</span></small>');
       }
       else {
-        fileName = e.target.value.split( '\\' ).pop();
-      }
-
-      if( fileName ) {
-        $('<small><i class="retraitespopulaires-icon retraitespopulaires-icon-documents-thick" aria-hidden="true"></i> <span>' +fileName +'</span></small>').insertAfter($handler);
-      }
-      else {
-        $label.text(labelVal);
+        for (let x = 0; x < files.length; x++) {
+          $files.append('<small><i class="retraitespopulaires-icon retraitespopulaires-icon-documents-thick" aria-hidden="true"></i> <span>' +files[x].name+'</span></small>');
+        }
       }
     });
   });
-
 }
