75ed9c176206a6e71cdd5cb64db6087e9b63d0dc
Kevin Wenger <wenger.kev@gmail.com>
2017-03-13 10:57:38 +0100

    Rework peering mandant-header for mobile

--- tasks/server.js
+++ tasks/server.js
@@ -1,43 +1,99 @@
 import gulp from 'gulp';
+import yargs from 'yargs';
 import config from '../gulp_config.json';
 import browserSync from 'browser-sync';
-import runSequence from 'run-sequence';
 
-import loadPlugins from 'gulp-load-plugins';
-const $ = loadPlugins();
-const reload = browserSync.reload;
+import { img } from './images';
+import { styles } from './styles';
+import { scripts } from './scripts';
+import { icons } from './icons';
 
-  /**
-   * Serve
-   */
-  export const serve = () => {
-     browserSync({
-       server: {
-         baseDir: [config.app.basedir]
-       },
-       open: false
-     });
-     gulp.watch([config.assets + 'sass/**/*.scss'], function() {
-       runSequence('styles', 'metalsmith', reload);
-     });
-     gulp.watch([config.assets + 'sass/styleguide.scss', config.assets + 'sass/styleguide-variables.scss'], function() {
-       runSequence('metalsmith-styles', 'metalsmith', reload);
-     });
-     gulp.watch([config.assets + 'img/**/*', config.assets + 'svg/**/*'], function() {
-       runSequence('img', 'metalsmith', reload);
-     });
-     gulp.watch([config.assets + 'icons/**/*'], function() {
-       runSequence('icons', 'metalsmith', reload);
-     });
-     gulp.watch([config.assets + 'js/**/*.js'], function() {
-       runSequence('scripts', 'metalsmith', reload);
-     });
-     gulp.watch([
-       config.assets + 'components/**/*.{html,hbs,md,swig}',
-       config.assets + 'templates/**/*.{html,hbs,md,swig}',
-       config.assets + 'docs/**/*.md',
-       config.assets + 'data/**/*.{json,yml}'
-     ], function() {
-       runSequence('metalsmith-docs', reload);
-     });
-  }
+/*
+ * Hot css injection
+ */
+const inject = () => {
+  return gulp.src([`${config.metalsmith.dist}/build/**/*.css`])
+    .pipe(browserSync.stream({match: '**/*.css'}));
+};
+
+/**
+ * Reload
+ */
+const reload = (done) => {
+  browserSync.reload();
+  done();
+};
+
+/**
+ * useless task
+ */
+const inprod = done => done();
+
+/**
+ * Serve
+ */
+export const serve = () => {
+  browserSync({
+    server: {
+      baseDir: [config.app.basedir],
+    },
+    notify: {
+      styles: {
+        padding: "5px",
+        fontSize: "0.7em",
+        top: 'auto',
+        bottom: 0,
+        borderBottomLeftRadius: 0,
+        borderTopLeftRadius: "5px",
+        backgroundColor: "#ef2678",
+      }
+    },
+    open: false
+  });
+
+  gulp.watch([
+    `${config.assets}sass/**/*.scss`
+  ], gulp.series(
+    styles,
+    yargs.argv.production ? inprod : require('./metalsmith').metalsmithAssets,
+    inject));
+
+  gulp.watch([
+    `${config.assets}sass/styleguide.scss`,
+    `${config.assets}sass/styleguide-variables.scss`
+  ], gulp.series(
+    yargs.argv.production ? inprod : require('./metalsmith').metalsmithStyles,
+    inject
+  ));
+
+  gulp.watch([
+    `${config.assets}img/**/*`,
+    `${config.assets}svg/**/*`
+  ], gulp.series(
+    img,
+    yargs.argv.production ? inprod : require('./metalsmith').metalsmith,
+    reload
+  ));
+
+  gulp.watch([
+    `${config.assets}icons/**/*`
+  ], gulp.series(
+    icons,
+    yargs.argv.production ? inprod : require('./metalsmith').metalsmith,
+    reload
+  ));
+
+  gulp.watch([
+    `${config.assets}js/**/*.js`
+  ], gulp.series(scripts));
+
+  gulp.watch([
+    `${config.assets}components/**/*.{html,hbs,md,swig}`,
+    `${config.assets}templates/**/*.{html,hbs,md,swig}`,
+    `${config.assets}docs/**/*.{html,hbs,md,swig}`,
+    `${config.assets}data/**/*.{json,yml}`
+  ], gulp.series(
+    yargs.argv.production ? inprod : require('./metalsmith').metalsmithDocs,
+    reload
+  ));
+};
