333a2fe2d853f0bf003b505dd5de35dbf97cb2f4
Clément Dieperink <c.dieperink@retraitespopulaires.ch>
2018-05-14 14:51:19 +0200

    Add module for authentication with LDAP

---
+++ web/modules/custom/retraitespopulaires/rp_auth/src/Routing/RouteSubscriber.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\rp_auth\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Listens to the dynamic rp_auth route events.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alterRoutes(RouteCollection $collection) {
+    // Change the title of /user/login page.
+    if ($route = $collection->get('user.login')) {
+      $route->setDefault('_title', 'Se connecter');
+    }
+
+    // Always deny access to '/user/{1}'.
+    // Note that the second parameter of setRequirement() is a string.
+    if ($route = $collection->get('entity.user.canonical')) {
+      $route->setRequirement('_access', 'FALSE');
+    }
+
+    // Always deny access to non-admin '/user/{*}/edit'.
+    if ($route = $collection->get('entity.user.edit_form')) {
+      $route->setRequirement('_permission', 'access administration pages');
+    }
+
+    // Always deny access to non-admin '/user/register'.
+    if ($route = $collection->get('user.register')) {
+      $route->setRequirement('_permission', 'access administration pages');
+    }
+
+    // Always deny access to '/user/password'.
+    if ($route = $collection->get('user.pass')) {
+      $route->setRequirement('_access', 'FALSE');
+    }
+  }
+
+}
