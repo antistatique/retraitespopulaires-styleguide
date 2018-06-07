d7c91d1a324e414d36ea692781918c91b4665e1d
Clément Dieperink <c.dieperink@retraitespopulaires.ch>
2018-06-07 16:08:00 +0200

    Correct rp_product

--- web/modules/custom/retraitespopulaires/rp_product/src/Plugin/Block/ProductsBlock.php
+++ web/modules/custom/retraitespopulaires/rp_product/src/Plugin/Block/ProductsBlock.php
@@ -1,8 +1,4 @@
 <?php
-/**
-* @file
-* Contains \Drupal\rp_product\Plugin\Block\ProductsBlock.
-*/
 
 namespace Drupal\rp_product\Plugin\Block;
 
@@ -14,83 +10,87 @@ use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Routing\CurrentRouteMatch;
 
 /**
-* Provides a 'Products' Block
-*
-* @Block(
-*   id = "rp_site_products_block",
-*   admin_label = @Translation("Products block"),
-* )
-*
-* Inline example:
-* <code>
-* load_block('rp_site_products_block')
-* </code>
-*/
+ * Provides a 'Products' Block.
+ *
+ * @Block(
+ *   id = "rp_site_products_block",
+ *   admin_label = @Translation("Products block"),
+ * )
+ *
+ * Inline example:
+ * <code>
+ * load_block('rp_site_products_block')
+ * </code>
+ */
 class ProductsBlock extends BlockBase implements ContainerFactoryPluginInterface {
 
-    /**
-    * EntityTypeManagerInterface to load Nodes
-    * @var EntityTypeManagerInterface
-    */
-    private $entity_node;
+  /**
+   * EntityTypeManagerInterface to load Nodes.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  private $entityNode;
 
-    /**
-    * Current Route
-    * @var CurrentRouteMatch
-    */
-    private $route;
+  /**
+   * Current Route.
+   *
+   * @var \Drupal\Core\Routing\CurrentRouteMatch
+   */
+  private $route;
 
-    /**
-    * Class constructor.
-    */
-    public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity, CurrentRouteMatch $route) {
-        parent::__construct($configuration, $plugin_id, $plugin_definition);
-        $this->entity_node   = $entity->getStorage('node');
-        $this->route         = $route;
-    }
+  /**
+   * Class constructor.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity, CurrentRouteMatch $route) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityNode = $entity->getStorage('node');
+    $this->route      = $route;
+  }
 
-    /**
-    * {@inheritdoc}
-    */
-    public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-        // Instantiates this form class.
-        return new static(
-            // Load the service required to construct this class.
-            $configuration,
-            $plugin_id,
-            $plugin_definition,
-            // Load customs services used in this class.
-            $container->get('entity_type.manager'),
-            $container->get('current_route_match')
-        );
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    // Instantiates this form class.
+    return new static(
+        // Load the service required to construct this class.
+        $configuration,
+        $plugin_id,
+        $plugin_definition,
+        // Load customs services used in this class.
+        $container->get('entity_type.manager'),
+        $container->get('current_route_match')
+    );
+  }
 
-    /**
-    * {@inheritdoc}
-    */
-    public function build($params = array()) {
-        $variables = array('products' => array());
+  /**
+   * {@inheritdoc}
+   */
+  public function build($params = []) {
+    $variables = ['products' => []];
 
-        if ($node = $this->route->getParameter('node')) {
-            if( isset($node->field_products) && !empty($node->field_products) ){
-                foreach ($node->field_products as $key => $product) {
-                  $variables['products'][] = $product->entity;
-                }
-            }
+    if ($node = $this->route->getParameter('node')) {
+      if (isset($node->field_products) && !empty($node->field_products)) {
+        foreach ($node->field_products as $product) {
+          $variables['products'][] = $product->entity;
         }
-
-        return [
-            '#theme'     => 'rp_site_products_block',
-            '#variables' => $variables,
-            '#cache' => [
-                'contexts' => [
-                    'url.path',
-                    'url.query_args'
-                ],
-                'tags' => [
-                    'node_list:product', // invalidated whenever any Node entity is updated, deleted or created
-                ],
-            ]
-        ];
+      }
     }
+
+    return [
+      '#theme'     => 'rp_site_products_block',
+      '#variables' => $variables,
+      '#cache' => [
+        'contexts' => [
+          'url.path',
+          'url.query_args',
+        ],
+        'tags' => [
+        // Invalidated whenever any Node entity is updated, deleted or created.
+          'node_list:product',
+        ],
+      ],
+    ];
+  }
+
 }
