f993be870b7e1fd601950d8782ed16e8c49067f4
Jérémy Corbaz <j.corbaz@retraitespopulaires.ch>
2018-11-15 09:47:15 +0100

    commented JS file and added space in scss

--- assets/js/ckeditor_accordion.js
+++ assets/js/ckeditor_accordion.js
@@ -1,5 +1,11 @@
 import $ from 'jquery';
 
+/**
+ * This JS is only use with the CKeditor Accordion module.
+ * It adds a div around the head of each tab of the accordion to make the whole head clickable.
+ *
+ *  @see {@link https://www.drupal.org/project/ckeditor_accordion}
+ */
 export function ckeditor_accordion() {
 
     $(document).ready(function () { // wait otherwise it's in conflict with the JS of module
