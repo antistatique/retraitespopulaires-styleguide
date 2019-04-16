840f900a5c45b3b620767962858e4cef0ee0cfaf
Clement Dieperink <c.dieperink@retraitespopulaires.ch>
2019-04-16 14:24:03 +0200

    correct some bug with slider and add possibility to generate slider after document.ready (e.g. after an ajax call)

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -12,118 +12,145 @@ export function slider () {
       });
 
       // Add slider to each div with slider class
-      $('.slider').each(function (index, element) {
-        // Get the JQuery element and the input with the slider
-        element = $(element);
-
-        // Get the data parameters
-        let input = element.closest('.form-group').find('input');
-        let currentValue = Number(input.length > 0 && input.value !== '' ? input.autoNumeric('get') : element.data('value'));
-        let maxValue = Number(element.data('max'));
-        let minValue = Number(element.data('min'));
-        let step = Number(element.data('step'));
-        let labels = element.data('labels');
-        let range = 'min';
-        let showPips = element.data('show-pips');
-
-        // Check if labels is an object
-        if (labels && typeof labels === 'string') {
-          console.error('The labels string seems to be badly declared.');
-          labels = null;
-        }
-
-        // The range type can be null so set value only if undefined
-        if (element.data('range') !== undefined) {
-          range = element.data('range');
-
-          // If range is null add class no-range.
-          if (range === 'no-range') {
-            range = null;
-            element.addClass('no-range');
-          }
-        }
-
-        // Init slider for this element
-        element.slider({
-          orientation: element.hasClass('slider-vertical') ? 'vertical' : 'horizontal',
-          range: range,
-          max: maxValue,
-          min: minValue,
-          step: step,
-          value: currentValue,
-          animate: 100,
-          disabled: element.hasClass('disabled'),
-          // For update input
-          slide: function (event, ui) {
-            // To be sure that slide don't go out of slider max value (sometimes it did when the last step wasn't a full step)
-            if (ui.value > maxValue) {
-              ui.value = maxValue;
-            }
-
-            if (input.length > 0) {
-              input.autoNumeric('set', ui.value);
-            }
-
-            // Add active class to the only label
-            element.children('.ui-slider-pip').removeClass('active');
-            element.children('.ui-slider-pip[data-active-value="' + ui.value + '"]').addClass('active');
-          }
-        });
-
-        // Show labels.
-        if (labels) {
-          // Add margin bottom.
-          element.addClass('labels');
-
-          for (let labelValue in labels) {
-            newPips(labelValue);
-          }
-        }
-
-        // Show pips on each step
-        if (showPips) {
-          for (let i = minValue; i <= maxValue; i += step) {
-            // Only show if there's no label at the position.
-            if (!labels || !(labels.hasOwnProperty(i))) {
-              newPips(i);
-            }
-          }
-        }
-
-        if (input.length > 0) {
-          // Listen change event for update slider
-          input.change(function () {
-            element.slider('value', input.autoNumeric('get'));
-          });
-        }
-
-        function newPips(value) {
-          value = Number(value);
-
-          // Calculate the position of the pip.
-          let percent = (value - minValue) / (maxValue - minValue) * 100 - 5;
-
-          // Create pip.
-          let newEl = $('<div class="ui-slider-pip" data-active-value="' + value + '"><span class="ui-slider-line"></span></div>')
-              .data('active-value', value)
-              .css('left', percent + '%');
-
-          // If there's a label add it to the pip.
-          if (labels[value]) {
-            newEl.append('<span class="ui-slider-label">' + labels[value] + '</span>');
-          }
-
-          // If it's the current selectionned value set to active.
-          if (value === currentValue) {
-            newEl.addClass('active');
-          }
-
-          // Add the pip to the slider.
-          element.append(newEl);
-        }
-      });
+      $('.slider').generateSlider();
     }
   });
 }
 
+(function ($) {
+  $.fn.generateSlider = function() {
+    this.each(function (index, element) {
+      // Get the JQuery element and the input with the slider
+      element = $(element);
 
+      // Get the data parameters
+      let input = element.closest('.form-group').find('input');
+      let currentValue = 0;
+      let maxValue = Number(element.data('max'));
+      let minValue = Number(element.data('min'));
+      let step = Number(element.data('step'));
+      let labels = element.data('labels');
+      let range = 'min';
+      let showPips = element.data('show-pips');
+      let isAutoNumeric = false;
+
+      // Get current value from input if there's is one otherwise get from data on slider.
+      if (input.length > 0 && input.val()) {
+        // Verify if autoNumeric is set on this input.
+        if (input.autoNumeric('getSettings')) {
+          currentValue = Number(input.autoNumeric('get'));
+          isAutoNumeric = true;
+        }
+        else {
+          currentValue = Number(input.val());
+        }
+      }
+      else {
+        currentValue = Number(element.data('value'));
+      }
+
+      // To be sure that it's a number.
+      if (isNaN(currentValue)) {
+        currentValue = 0;
+      }
+
+
+      // Check if labels is an object
+      if (labels && typeof labels === 'string') {
+        console.error('The labels string seems to be badly declared.');
+        labels = null;
+      }
+
+      // The range type can be null so set value only if undefined
+      if (element.data('range') !== undefined) {
+        range = element.data('range');
+
+        // If range is null add class no-range.
+        if (range === 'no-range') {
+          range = null;
+          element.addClass('no-range');
+        }
+      }
+
+      // Init slider for this element
+      element.slider({
+        orientation: element.hasClass('slider-vertical') ? 'vertical' : 'horizontal',
+        range: range,
+        max: maxValue,
+        min: minValue,
+        step: step,
+        value: currentValue,
+        animate: 100,
+        disabled: element.hasClass('disabled'),
+        // For update input
+        slide: function (event, ui) {
+          // To be sure that slide don't go out of slider max value (sometimes it did when the last step wasn't a full step)
+          if (ui.value > maxValue) {
+            ui.value = maxValue;
+          }
+
+          if (input.length > 0) {
+            isAutoNumeric ? input.autoNumeric('set', ui.value) : input.val(ui.value);
+          }
+
+          // Add active class to the only label
+          element.children('.ui-slider-pip').removeClass('active');
+          element.children('.ui-slider-pip[data-active-value="' + ui.value + '"]').addClass('active');
+        }
+      });
+
+      // Show labels.
+      if (labels) {
+        // Add margin bottom.
+        element.addClass('labels');
+
+        for (let labelValue in labels) {
+          newPips(labelValue);
+        }
+      }
+
+      // Show pips on each step
+      if (showPips) {
+        for (let i = minValue; i <= maxValue; i += step) {
+          // Only show if there's no label at the position.
+          if (!labels || !(labels.hasOwnProperty(i))) {
+            newPips(i);
+          }
+        }
+      }
+
+      if (input.length > 0) {
+        // Listen change event for update slider
+        input.change(function () {
+          let val = isAutoNumeric ? input.autoNumeric('get') : input.val();
+          element.slider('value', val);
+        });
+      }
+
+      // Function to generate the pips.
+      function newPips(value) {
+        value = Number(value);
+
+        // Calculate the position of the pip.
+        let percent = (value - minValue) / (maxValue - minValue) * 100 - 5;
+
+        // Create pip.
+        let newEl = $('<div class="ui-slider-pip" data-active-value="' + value + '"><span class="ui-slider-line"></span></div>')
+            .css('left', percent + '%');
+
+        // If there's a label add it to the pip.
+        if (labels[value]) {
+          newEl.append('<span class="ui-slider-label">' + labels[value] + '</span>');
+        }
+
+        // If it is the current selected value set to active.
+        if (value === currentValue) {
+          newEl.addClass('active');
+        }
+
+        // Add the pip to the slider.
+        element.append(newEl);
+      }
+    });
+  };
+})(jQuery);
