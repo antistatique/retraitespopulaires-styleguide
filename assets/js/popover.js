a5604a301ce5ee41cd4094e976f29f0eefdd3575
Aleh Chyzhyk <a.chyzhyk@retraitespopulaires.ch>
2020-04-06 10:21:27 +0200

    MD-610: popover on hover modification

--- assets/js/popover.js
+++ assets/js/popover.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 
 export function popover () {
-  $('[data-toggle="popover"]').popover();
+ // $('[data-toggle="popover"]').popover();
+  $('[data-toggle="popover"]').popover({ trigger: 'hover' }); //For optional on hover
 }
